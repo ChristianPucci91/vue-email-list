342ceffe83870f7c290950c63ccaf1d0e6d3cfa0
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-30 14:57:54 +0100

    code cleaning

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@ var app = new Vue({
   },
   mounted: function () {
 
-    for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
 
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail ')
-      .then(risposta => {
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail ')
+     .then(risposta => {
 
-         this.email = risposta.data.response;
-         this.mailList.push(this.email)
+       this.email = risposta.data.response;
+       this.mailList.push(this.email)
 
-       });
+      });
     }
    }
 });
 
-// per generare 10 mail metteremo all'interno di un ciclo for che ciclerà 10 volte un endpoint di un API che otterrà delle mail random. 
+// per generare 10 mail metteremo all'interno di un ciclo for che ciclerà 10 volte un endpoint di un API che otterrà delle mail random.
 // Ogni ciclo otteniamo una mail che andiamo a pushare all'interno di un array.
