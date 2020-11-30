0b7f788b9ac99d6fef66bdd34064bd79a0cc9479
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-30 14:50:32 +0100

    Esercizio completo.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Genera 10 mail tramite api e stampale in una listas
+
+
+// Avvio e dichiaro vue nel container
+var app = new Vue({
+  el: "#root",
+  data: {
+    mailList: []
+  },
+  mounted: function () {
+
+    for (var i = 0; i < 10; i++) {
+
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail ')
+      .then(risposta => {
+
+         this.email = risposta.data.response;
+         this.mailList.push(this.email)
+
+       });
+    }
+   }
+});
+
+// per generare 10 mail metteremo all'interno di un ciclo for che ciclerà 10 volte un endpoint di un API che otterrà delle mail random. 
+// Ogni ciclo otteniamo una mail che andiamo a pushare all'interno di un array.
